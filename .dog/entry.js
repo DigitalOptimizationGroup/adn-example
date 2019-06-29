536130ed631a3885b74e574880d498432bac5c1d
Shalom Volchok <shalomvolchok@gmail.com>
2019-06-29 19:06:33 +0300

    remove unwanted files

--- .dog/entry.js
+++
@@ -1,5 +0,0 @@
-import config from "../dog-app-config.json";
-import { setup } from "@digitaloptgroup/adn-apps-cra/lib/script";
-import assets from "./client-assets";
-
-export const handler = setup({ ...config, assets });
